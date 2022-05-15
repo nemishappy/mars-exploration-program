f2679865096d687a572a38200dfe6420c67ae313
nemishappy <neomk24@gmail.com>
2022-05-15 23:18:41 +0700

    validation input

--- pages/getstart.tsx
+++ pages/getstart.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import Layout from "../components/layout";
-import { getPostData } from "../lib/mission";
 import { GetStaticProps, GetStaticPaths } from "next";
+import utilStyles from "../styles/utils.module.css";
 
+let nextId = 1;
+const initialTasks = [
+  { id: 0, direction: "N", posX: "0", posY: "0", reachEdge: false },
+];
 const GetStart = () => {
-  const [inputs, setInputs] = useState<string[]>([]);
+  const [isValid, setValid] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [range, setRange] = useState(0);
+  const [output, setOutput] = useState(initialTasks);
+
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const fileReader = new FileReader();
     const { files } = e.currentTarget;
@@ -12,30 +20,69 @@ const GetStart = () => {
       fileReader.readAsText(files[0], "UTF-8");
       fileReader.onload = (e) => {
         const content = e.target!.result;
-        const inputs = (content as string).split(/\r\n|\r|\n/);
-        if (vaildInputs(inputs)) {
-          setInputs(inputs);
+        const lines = (content as string).split(/\r\n|\r|\n/);
+
+        if (validInputs(lines)) {
+          setErrMsg("");
+          processInputs(lines);
+        } else {
+          console.log("Invalid input");
+          setErrMsg(
+            "This is mars exploration mission every input must be correct."
+          );
         }
         console.log(content);
-        console.log(inputs);
       };
     }
   };
+  function validInputs(lines: string[]): boolean {
+    if (lines[0] && !isNaN(Number(lines[0]))) {
+      for (let index = 1; index < lines.length; index++) {
+        const element = lines[index];
+        switch (element) {
+          case "F":
+            console.log(true);
+            break;
+
+          case "L":
+            console.log(true);
+            break;
+
+          case "R":
+            console.log(true);
+            break;
+
+          case "A":
+            console.log(true);
+            break;
+
+          default:
+            setValid(false);
+            return false;
+        }
+      }
+      setValid(true);
+      return true;
+    }
+    setValid(false);
+    return false;
+  }
+
+  function processInputs(lines: string[]): void {
+    lines.forEach(function (value) {
+      console.log(value);
+    });
+  }
+
   return (
     <Layout>
-      <div>
-        {inputs}
+      <div className={utilStyles.dFlexCol}>
         <input accept="text/plain" type="file" onChange={handleChange} />
+        <button disabled={!isValid}>Process</button>
       </div>
+      {errMsg ? <p className={utilStyles.error}>{errMsg}</p> : <></>}
     </Layout>
   );
 };
-
-function vaildInputs(inputs: string[]): boolean {
-  if (inputs.length != 0) return true;
-  return false;
-}
-
-function processInputs(): void {}
 
 export default GetStart;
